30cc343a62e81ff7ee15919d2700508a191b6235
Ryan James Whitehouse <r.whitehouse@klarna.com>
2022-01-23 23:50:19 +0100

    Add front end

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -2,6 +2,7 @@ import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { SolanaTwitter } from '../target/types/solana_twitter';
 import * as assert from "assert";
+import * as bs58 from "bs58";
 
 describe('solana-twitter', () => {
   // Configure the client to use the local cluster.
@@ -117,5 +118,52 @@ describe('solana-twitter', () => {
     }
 
     assert.fail('The instruction should have failed with a 281-character content.');
+  });
+
+  it('can fetch all tweets', async () => {
+    const tweetAccounts = await program.account.tweet.all();
+    assert.ok(tweetAccounts.length >= 3);
+  });
+
+  it('can filter tweets by author', async () => {
+    const authorPublicKey = program.provider.wallet.publicKey
+    const tweetAccounts = await program.account.tweet.all([
+        {
+            memcmp: {
+                offset: 8, // Discriminator.
+                bytes: authorPublicKey.toBase58(),
+            }
+        }
+    ]);
+
+    const allTweets = await program.account.tweet.all();
+    
+    assert.ok(allTweets.length > tweetAccounts.length);
+    assert.ok(tweetAccounts.every(tweetAccount => {
+      return tweetAccount.account.author.toBase58() === authorPublicKey.toBase58()
+    }))
+  });
+
+  it('can filter tweets by topics', async () => {
+    const TOPIC_OFFSET = 8 + // Discriminator.
+    32 + // Author public key.
+    8 + // Timestamp.
+    4; // Topic string prefix.
+    
+    const tweetAccounts = await program.account.tweet.all([
+        {
+            memcmp: {
+                offset: TOPIC_OFFSET,
+                bytes: bs58.encode(Buffer.from('veganism')),
+            }
+        }
+    ]);
+
+    const allTweets = await program.account.tweet.all();
+    
+    assert.ok(allTweets.length > tweetAccounts.length);
+    assert.ok(tweetAccounts.every(tweetAccount => {
+        return tweetAccount.account.topic === 'veganism'
+    }))
   });
 });
